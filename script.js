207b3b34046f9e26c5b9dc327ae1bd8ed30f479d
Mahipal73 <116993931+Mahipal73@users.noreply.github.com>
2023-06-02 00:50:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const billInput = document.getElementById('billTotalInput')
 const tipInput = document.getElementById('tipInput')
 const numberOfPeopleDiv = document.getElementById('numberOfPeople')
-const perPersonTotal = document.getElementById('perPersonTotal')
+const perPersonTotalDiv = document.getElementById('perPersonTotal')
 
 let numberOfPeople = Number(numberOfPeopleDiv.innerText)
 
@@ -11,9 +11,24 @@ const calculateBill = ( )  => {
   const tipAmount = bill * tipPercentage
   const total = tipAmount+bill
 
-  
+  const perPersonTotal = total/numberOfPeople
+  perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}` 
 }
-const increasePeople = ( ) => { }
-const decreasePeople = ( ) => { }
+const increasePeople = ( ) => {
+  numberOfPeople+=1
+  numberOfPeopleDiv.innerText = numberOfPeople
+  calculateBill()
+}
+const decreasePeople = ( ) => {
+  if(numberOfPeople<=1){
+    throw 'Hey! you cannot have less than 1 person!'
+    return 
+  }
+  numberOfPeople-=1
+  numberOfPeopleDiv.innerText = numberOfPeople
+  calculateBill()
+}
+
+
 
 
